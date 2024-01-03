d029b9e049abead98fb7514dd06a92ea6924f0bc
renusaini12 <155060905+renusaini12@users.noreply.github.com>
2024-01-03 17:34:21 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,10 @@
+function popup(){
+    let a=document.getElementById("o")
+    o.style.display="flex";
+    
+}
+
+function pop(){
+    let b =document.getElementById("o")
+    b.style.display="none"
+}
